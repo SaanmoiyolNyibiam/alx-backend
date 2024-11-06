a9841821f9c08ecc1e9f853682ad8357d083c75b
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-11-06 09:43:27 +0100

    style(alx-backend:0x02): add new line before and after function definition to conform to pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,12 +6,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """
     This is a function that returns the index page of the application
     """
     return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run()
