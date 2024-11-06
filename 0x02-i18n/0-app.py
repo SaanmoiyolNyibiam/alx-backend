428849d8f75e111ae5040a34a2eaaaa1499d435b
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-11-06 09:33:29 +0100

    build(alx-backend:0x02): add solution to task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a module that defines a simple flask application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """
+    This is a function that returns the index page of the application
+    """
+    return render_template("0-index.html")
+
+if __name__ == "__main__":
+    app.run()
