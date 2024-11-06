01e5718035f229bfa60dc81609d076116b6cb1e5
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-11-06 10:37:56 +0100

    build(alx-backend:0x02): add solution to task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+This is a module that basically sets up babel
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config():
+    """
+    Holds configuration values for the
+    flask application
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# applies the configuration to the flask instance
+app.config.from_object(Config)
+# intializes babel with the configuration values set in the config class
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """
+    This is a function that returns the index page of the application
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
